0fb1b4c02a77dab1a88d84d0a342de8d7f885501
victorjunior95 <victorjr.muchacho@gmail.com>
2021-01-05 20:10:35 -0300

    Exercicio 2 - testar a funçao getUserName como promisse

---
+++ FUNDAMENTOS-DESENVOLVIMENTO-WEB/BLOCO_10/DIA_2/exercises/exercise_2_3.test.js
@@ -0,0 +1,19 @@
+const getUserName = require('./exercise_2_3')
+// Utilizando a sintaxe de Promise , faça um teste que verifique o resultado da função getUserName para o caso em que o usuário é encontrado, e também um teste para o caso em que o usuário não é encontrado.
+// Dica : Veja os dados falsos utilizados no banco de dados, disponíveis na variável users , para saber quais IDs existem.
+describe('Verifica o resultado da função getUserName (Promisse)', () => {
+  it('caso em que o usuário é encontrado.', () => {
+    return getUserName(4).then(user => {
+      expect(user).toEqual('Mark');
+    });
+  });
+
+  it('caso em que o usuário não é encontrado.', () => {
+    return getUserName(3).catch(error => {
+      expect(error).toEqual({ error: 'User with 3 not found.' })
+    });
+  });
+});
+
+// Reescreva o teste do exercício anterior, desta vez utilizando a sintaxe de async/await .
+// Dica: Utilize o try/catch para o caso de erro.
