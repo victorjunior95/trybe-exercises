7179b341903036c5827df3fe95e9337faf3b7649
victorjunior95 <victorjr.muchacho@gmail.com>
2021-01-06 14:20:02 -0300

    Exercicio 5 - exercitar o funcionamento do beforeEach e afterEach por meio de console.log

--- FUNDAMENTOS-DESENVOLVIMENTO-WEB/BLOCO_10/DIA_2/exercises/exercise_5.test.js
+++ FUNDAMENTOS-DESENVOLVIMENTO-WEB/BLOCO_10/DIA_2/exercises/exercise_5.test.js
@@ -1,1 +1,29 @@
 // Para este exercício, tente adivinhar a saída dos console.log dos testes abaixo sem executá-los, e veja se compreendeu bem o funcionamento do beforeEach e do afterEach .
+beforeEach(() => console.log('1 - beforeEach'));
+afterEach(() => console.log('1 - afterEach'));
+
+test('1', () => console.log('1 - test'));
+
+describe('Scoped / Nested block', () => {
+  beforeEach(() => console.log('2 - beforeEach'));
+  afterEach(() => console.log('2 - afterEach'));
+
+  test('2', () => console.log('2 - test'));
+});
+
+// test 1
+//  -> '1 - beforeEach'
+//  '1'
+//     -> '1 - test'
+//  -> '1- afterEach'
+
+// -> '1 - beforeEach'
+
+// test 2
+// 'Scoped / Nested Block'
+//  -> '2 - beforeEach'
+//  '2'
+//     -> '2 - test'
+//  -> '2- afterEach'
+
+// -> '1 - afterEach'
